465098d7d3bae2b6932d0ba2ac49d37b029c533f
Christophe Phillips <c.phillips@uliege.be>
2019-11-15 17:26:29 +0100

    First version of data generation routine
    Generate some profile of signal and tissue probabilities.

---
+++ cp_create_data.m
@@ -0,0 +1,63 @@
+function out = cp_create_data
+% Function to create some synthetic 1D data:
+% - some "image" values, supposed to be like T1w or MT
+% - some tissue probabilities for GM, WM & CSF
+%__________________________________________________________________________
+% Copyright (C) 2019 GIGA Institute
+
+% Written by C. Phillips, 2019.
+% Cyclotron Research Centre, University of Liege, Belgium
+
+%% Key parameters
+% for GM, WM & CSF respectively
+T_names  = {'GM', 'WM', 'CSF'};
+T_signal = [50 100 5];
+T_noise  = [2 2 10];
+T_Wsegm  = [32 16 24 26 24 8 12 8 12 4 32]; % width of tissue segments
+T_probGmWmCsf = [ ...
+     1 98  2  2  2 95  5  2  5 98  1 ; ...
+     1  1 97  2 97  2 94  4 94  1  1 ; ...
+    98  1  1 96  1  3  1 95  1  1 98]; % tissue prob per segment
+T_tcnoise = 2; %error on tissue class probability
+T_tcthresh = .1; % minimum tissue class prob in % !
+
+T_index = cumsum([1 T_Wsegm]); T_index(end) = T_index(end)+1;
+Nsegm = numel(T_Wsegm);
+
+%% Creating data and tissue profiles
+P_GmWmCsf = zeros(3,max(T_index)-1);
+P_signal  = zeros(1,max(T_index)-1);
+for ii=1:Nsegm
+    l_ind = T_index(ii):T_index(ii+1)-1; % indexes
+    for jj=1:3 % 3 tissue probabily profiles
+        % exact tissue prob
+        P_GmWmCsf(jj,l_ind) = T_probGmWmCsf(jj,ii);
+        % signal from tissue prob + noise
+        P_signal(l_ind) = P_signal(l_ind) + ...
+            T_probGmWmCsf(jj,ii)/100*T_signal(jj) + ...
+            T_probGmWmCsf(jj,ii)/100.*randn(1,numel(l_ind))*T_noise(jj) ;
+    end
+end
+% add noise to tissue prob -> need to ensure p>0 and sum==1
+P_GmWmCsf = P_GmWmCsf + randn(size(P_GmWmCsf))*T_tcnoise;
+% minimum value should not be lower than .1%
+P_GmWmCsf(P_GmWmCsf<T_tcthresh) = T_tcthresh
+% P_GmWmCsf = P_GmWmCsf + ones(3,1)*min(P_GmWmCsf) + .1; 
+% sum to 1
+P_GmWmCsf = P_GmWmCsf ./ (ones(3,1)*sum(P_GmWmCsf)) ; 
+
+%% Plot all profiles
+figure,
+% display intensity profile
+subplot(2,1,1)
+plot(P_signal)
+ylabel('Intensities')
+
+% display tissue probability profile
+for ii=1:3
+    subplot(6,1,3+ii)
+    plot(P_GmWmCsf(ii,:))
+    ylabel(T_names{ii})
+end
+
+end
